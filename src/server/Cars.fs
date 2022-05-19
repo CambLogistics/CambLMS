674a9b87e0abd3b91815179fe2b9d3e51f42e395
OrkenWhite <orkenwhite@disroot.org>
2022-05-19 15:43:51 +0200

    Added server-side car management code

---
+++ src/server/Cars.fs
@@ -0,0 +1,93 @@
+namespace camblms
+
+open WebSharper
+
+[<JavaScript>]
+type Car = {Id:string;CarType: string;RegNum: string;
+            AirRide: bool;GPS:bool;ParkTicket: bool;
+            Engine: int;ECU: int;Brakes: int;
+            Suspension: int;WeightReduction: int;Gearbox: int
+            Turbo: int; Tyres:int
+            KeyHolder1: Member option;KeyHolder2: Member option}
+
+module Cars =
+    let getTuningLevels =
+        try
+            let db = Database.SqlConnection.GetDataContext()
+            query{
+                for tuning in db.Camblogistics.TuningLevels do
+                select (tuning.Level,tuning.Name)
+            } |> Map.ofSeq
+        with
+            _ -> Map.ofList []
+    let getCars sid =
+        if not (User.verifyAdmin sid) then []
+        else
+        try
+        let db = Database.SqlConnection.GetDataContext()
+        query{
+            for car in db.Camblogistics.Cars do
+            join user1 in db.Camblogistics.Users on (car.KeyHolder1 = Some user1.Id)
+            join user2 in db.Camblogistics.Users on (car.KeyHolder2 = Some user2.Id)
+            select(
+                {
+                    Id = car.Id
+                    RegNum = car.RegNum
+                    CarType = car.Type
+                    KeyHolder1 = 
+                        if car.KeyHolder1.IsSome then 
+                            Some {Id = user1.Id;Name = user1.Name;Role = user1.Role;AccountID = user1.AccountId;Email = user1.Email} 
+                            else None
+                    KeyHolder2 = 
+                        if car.KeyHolder2.IsSome then 
+                            Some {Id = user2.Id;Name = user2.Name;Role = user2.Role;AccountID = user2.AccountId;Email = user2.Email} 
+                            else None
+                    AirRide = car.AirRide = (sbyte 1)
+                    ParkTicket = car.Ticket = (sbyte 1) 
+                    GPS = car.AirRide = (sbyte 1)
+                    Engine = car.Engine
+                    ECU = car.Ecu
+                    Brakes = car.Brakes
+                    Suspension = car.Suspension
+                    Gearbox = car.Gearbox
+                    Tyres = car.Tyres
+                    Turbo = car.Turbo
+                    WeightReduction = car.WeightReduction
+                }
+            )
+        } |> Seq.toList
+        with
+            _ -> []
+    let setCar sid car =
+        if not (User.verifyAdmin sid) then ()
+        else
+        try
+            let db = Database.SqlConnection.GetDataContext()
+            let existing =
+                query{
+                    for c in db.Camblogistics.Cars do
+                    where(c.Id = car.Id)
+                    select c          
+                } |> Seq.toList
+            let newCar = 
+                if List.isEmpty existing then db.Camblogistics.Cars.Create() else List.item 0 existing
+            newCar.Id <- car.Id
+            newCar.Type <- car.CarType
+            newCar.RegNum <- car.RegNum
+            newCar.KeyHolder1 <- if car.KeyHolder1.IsSome then Some car.KeyHolder1.Value.Id else None
+            newCar.KeyHolder2 <- if car.KeyHolder2.IsSome then Some car.KeyHolder2.Value.Id else None
+            newCar.AirRide <- if car.AirRide then sbyte 1 else sbyte 0
+            newCar.Ticket <- if car.ParkTicket then sbyte 1 else sbyte 0
+            newCar.Gps <- if car.GPS then sbyte 1 else sbyte 0
+            newCar.Ecu <- car.ECU
+            newCar.Engine <- car.Engine
+            newCar.Brakes <- car.Brakes
+            newCar.Gearbox <- car.Gearbox
+            newCar.Suspension <- car.Suspension
+            newCar.Turbo <- car.Turbo
+            newCar.Tyres <- car.Tyres
+            newCar.WeightReduction <- car.WeightReduction
+            db.SubmitUpdates()
+        with
+            _ -> ()
+    
--- src/server/Database.fs
+++ src/server/Database.fs
@@ -3,4 +3,4 @@ namespace camblms
 open FSharp.Data.Sql
 
 module Database =
-    type SqlConnection = SqlDataProvider<Common.DatabaseProviderTypes.MYSQL,  "Server=localhost;Database=camblogistics;Uid=camblms;Pwd=V3l3tlen_J3lsz0;">
+    type SqlConnection = SqlDataProvider<Common.DatabaseProviderTypes.MYSQL,  "Server=localhost;Database=camblogistics;Uid=camblms;Pwd=V3l3tlen_J3lsz0;",UseOptionTypes = true>
