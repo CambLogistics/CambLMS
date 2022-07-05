9dc84620636f1e337d1244048ac84518b7112168
OrkenWhite <orkenwhite@disroot.org>
2022-07-05 17:02:18 +0200

    Pressing enter works on login and registration page

--- src/sites/LoginPage.fs
+++ src/sites/LoginPage.fs
@@ -5,39 +5,48 @@ open WebSharper.JavaScript
 
 [<JavaScript>]
 module LoginPage =
-    let RenderPage() =
-        if JS.Window.Location.Href.Contains "registered" then Feedback.giveFeedback false "Regisztrációd sikeres és hamarosan jóváhagyásra kerül!"
-        SiteParts.LoginPage()
+    let callLogin name password rememberme =
+        async {
+            let! result = UserCallable.doLogin name password
+            match result with
+            | LoginResult.Success id ->
+                JS.Document.Cookie <- "clms_sid=" + id
+                if rememberme then
+                    JavaScript.Cookies.Set("clms_rm", "true")
+                    JavaScript.Cookies.Set("clms_rm_name", name)
+                else
+                    JavaScript.Cookies.Expire("clms_rm_name")
+                    JavaScript.Cookies.Set("clms_rm", "false")
+                JS.Window.Location.Replace "/"
+            | LoginResult.CredentialError -> Feedback.giveFeedback true "Hibás felhasználónév vagy jelszó!"
+            | LoginResult.DatabaseError -> Feedback.giveFeedback true "Adatbázishiba. Értesítsd a (műszaki) igazgatót!"
+            | LoginResult.NotApproved ->
+                Feedback.giveFeedback true "Regisztrációd még nem lett jóváhagyva. Kérjük légy türelemmel!"
+        }
+    let RenderPage () =
+        if JS.Window.Location.Href.Contains "registered" then
+            Feedback.giveFeedback false "Regisztrációd sikeres és hamarosan jóváhagyásra kerül!"
+        SiteParts
+            .LoginPage()
             .RememberMe(
-                match JavaScript.Cookies.Get "clms_rm" |> Optional.toOption with
-                    |None -> false
-                    |Some s -> s = "true"
+                match JavaScript.Cookies.Get "clms_rm"
+                      |> Optional.toOption
+                    with
+                | None -> false
+                | Some s -> s = "true"
             )
             .Name(
-                match JavaScript.Cookies.Get "clms_rm_name" |> Optional.toOption with
-                    |None -> ""
-                    |Some s -> s
+                match JavaScript.Cookies.Get "clms_rm_name"
+                      |> Optional.toOption
+                    with
+                | None -> ""
+                | Some s -> s
             )
-            .Login(
+            .Login(fun e ->
+                callLogin e.Vars.Name.Value e.Vars.Password.Value e.Vars.RememberMe.Value
+                |> Async.Start)
+            .Kblogin(
                 fun e ->
-                    async{
-                        let! result = UserCallable.doLogin e.Vars.Name.Value e.Vars.Password.Value
-                        match result with
-                            |LoginResult.Success id -> 
-                                JS.Document.Cookie <- "clms_sid=" + id
-                                if e.Vars.RememberMe.Value then
-                                    JavaScript.Cookies.Set("clms_rm","true")
-                                    JavaScript.Cookies.Set("clms_rm_name",e.Vars.Name.Value)
-                                else
-                                    JavaScript.Cookies.Expire("clms_rm_name")
-                                    JavaScript.Cookies.Set("clms_rm","false") 
-                                JS.Window.Location.Replace "/"
-                            |LoginResult.CredentialError ->
-                                Feedback.giveFeedback true "Hibás felhasználónév vagy jelszó!"
-                            |LoginResult.DatabaseError ->
-                                Feedback.giveFeedback true "Adatbázishiba. Értesítsd a (műszaki) igazgatót!"
-                            |LoginResult.NotApproved ->
-                                Feedback.giveFeedback true "Regisztrációd még nem lett jóváhagyva. Kérjük légy türelemmel!"
-                    } |> Async.Start
+                    if e.Event.KeyCode = 13 then callLogin e.Vars.Name.Value e.Vars.Password.Value e.Vars.RememberMe.Value |> Async.Start
             )
             .Doc()
--- src/sites/RegistrationPage.fs
+++ src/sites/RegistrationPage.fs
@@ -5,29 +5,39 @@ open WebSharper.JavaScript
 
 [<JavaScript>]
 module RegistrationPage =
-    let RenderPage() =
-        SiteParts.RegistrationPage()
-            .Register(
-                fun e ->
-                    async{
-                        if e.Vars.Password.Value = e.Vars.PasswordRepeat.Value && not (JS.IsNaN e.Vars.AccID.Value)
-                            then
-                                let! result = 
-                                    UserCallable.doRegister e.Vars.Name.Value e.Vars.Password.Value (int e.Vars.AccID.Value) e.Vars.Email.Value
-                                match result with
-                                    |RegisterResult.Success ->
-                                        JS.Window.Location.Replace "/login?registered=true"
-                                    |Exists ->
-                                        Feedback.giveFeedback true "Ilyen felhasználó már létezik!"
-                                    |RegisterResult.DatabaseError ->
-                                        Feedback.giveFeedback true "Adatbázishiba. Kérjük értesítsd a (műszaki) igazgatót!"
-                                    |MissingData ->
-                                        Feedback.giveFeedback true "Ellenőrizd a bevitt adatokat!"
-                                    |BadName ->
-                                        Feedback.giveFeedback true "Nem megfelelő név!"
-                                    |BadPassword ->
-                                        Feedback.giveFeedback true "Nem megfelelő jelszó!"
-                        else Feedback.giveFeedback true "Ellenőrizd a bevitt adatokat!"
-                    } |> Async.Start
-            )
+    let register name password passwordrepeat accid email =
+        async {
+            if password = passwordrepeat && not (JS.IsNaN accid) then
+                let! result = UserCallable.doRegister name password (int accid) email
+                match result with
+                | RegisterResult.Success -> JS.Window.Location.Replace "/login?registered=true"
+                | Exists -> Feedback.giveFeedback true "Ilyen felhasználó már létezik!"
+                | RegisterResult.DatabaseError ->
+                    Feedback.giveFeedback true "Adatbázishiba. Kérjük értesítsd a (műszaki) igazgatót!"
+                | MissingData -> Feedback.giveFeedback true "Ellenőrizd a bevitt adatokat!"
+                | BadName -> Feedback.giveFeedback true "Nem megfelelő név!"
+                | BadPassword -> Feedback.giveFeedback true "Nem megfelelő jelszó!"
+            else
+                Feedback.giveFeedback true "Ellenőrizd a bevitt adatokat!"
+        }
+    let RenderPage () =
+        SiteParts
+            .RegistrationPage()
+            .Kbregister(fun e ->
+                if e.Event.KeyCode = 13 then
+                    register
+                        e.Vars.Name.Value
+                        e.Vars.Password.Value
+                        e.Vars.PasswordRepeat.Value
+                        e.Vars.AccID.Value
+                        e.Vars.Email.Value
+                    |> Async.Start)
+            .Register(fun e ->
+                register
+                    e.Vars.Name.Value
+                    e.Vars.Password.Value
+                    e.Vars.PasswordRepeat.Value
+                    e.Vars.AccID.Value
+                    e.Vars.Email.Value
+                |> Async.Start)
             .Doc()
