5bf45d1bd4d96c6d6a5c114ac20517b9cf448d14
OrkenWhite <orkenwhite@disroot.org>
2022-08-09 19:21:14 +0200

    Added forgot password functionality

--- src/sites/ForgotPassPage.fs
+++ src/sites/ForgotPassPage.fs
@@ -1,9 +1,24 @@
 namespace camblms
 
-open WebSharper
-open WebSharper.UI
-
 module ForgotPassPage =
-        let RenderPage() =
-                SiteParts.ForgotPassTemplate()
-                        .Doc()
+    let callForgotPassHandler email =
+        async {
+            let! result = ForgotPass.changePasswordByEmail email
+
+            match result with
+            | MailSent -> Feedback.giveFeedback false "A levél az új jelszóval elküldésre került!"
+            | MailError -> Feedback.giveFeedback true "Hiba a levelezőrendszerben! Értesítsd a (műszaki) igazgatót!"
+            | ForgotPassStatus.DatabaseError ->
+                Feedback.giveFeedback true "Adatbázishiba! Értesítsd a (műszaki) igazgatót!"
+            | NoSuchUser -> Feedback.giveFeedback true "Nincs ezen e-mail címmel regisztrált felhasználó!"
+        }
+        |> Async.Start
+
+    let RenderPage () =
+        SiteParts
+            .ForgotPassTemplate()
+            .Submit(fun e -> callForgotPassHandler e.Vars.Email.Value)
+            .Kbemail(fun e ->
+                if e.Event.KeyCode = 13 then
+                    callForgotPassHandler e.Vars.Email.Value)
+            .Doc()
