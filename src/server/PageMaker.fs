ee75cfbeb4efc55c5e3fb23a11d914a2c706ad06
OrkenWhite <orkenwhite@disroot.org>
2022-08-09 19:52:58 +0200

    Reverted to old routing/page rendering

--- src/server/PageMaker.fs
+++ src/server/PageMaker.fs
@@ -6,13 +6,127 @@ open WebSharper.UI.Html
 open WebSharper.Sitelets
 
 module PageMakers =
-    let NormalPage ctx isServerSide isAdmin renderer passToRenderer =
+    let RegistrationAdmin ctx =
         SiteTemplates.MainTemplate()
-            .Stylesheet(if isAdmin then SiteTemplates.AdminStyle else SiteTemplates.NormalStyle)
-            .Navbar(Navbar.MakeNavbar ctx isAdmin)
-            .Main(if isServerSide then renderer passToRenderer else client <@renderer passToRenderer@>)
+            .Stylesheet(SiteTemplates.AdminStyle)
+            .Navbar(Navbar.MakeNavbar ctx true)
+            .Main(client <@RegistrationAdminPage.RenderPage()@>)
             .Doc()
-    let LoginPage ctx renderer passtoRenderer =
+    let PasswordChange ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.ChangeStyle)
+            .Navbar(Navbar.MakeNavbar ctx false)
+            .Main(client <@PasswordChangePage.RenderPage()@>)
+            .Doc()
+    let NameChange ctx =
+         SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.ChangeStyle)
+            .Navbar(Navbar.MakeNavbar ctx false)
+            .Main(client <@NameChangePage.RenderPage()@>)
+            .Doc()
+    let MembersAdmin ctx user =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.AdminStyle)
+            .Navbar(Navbar.MakeNavbar ctx true)
+            .Main(client <@MemberAdminPage.RenderPage user@>)
+            .Doc()
+    let Changelog ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.NormalStyle)
+            .Navbar(Navbar.MakeNavbar ctx false)
+            .Main(SiteParts.ChangelogTemplate().Doc())
+            .Doc()
+    let CarsAdmin ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.AdminStyle)
+            .Navbar(Navbar.MakeNavbar ctx true)
+            .Main(client <@CarsAdmin.RenderPage()@>)
+            .Doc()
+    let Information ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.NormalStyle)
+            .Navbar(Navbar.MakeNavbar ctx false)
+            .Main(Information.RenderPage ctx)
+            .Doc()
+    let Taxi ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.NormalStyle)
+            .Navbar(Navbar.MakeNavbar ctx false)
+            .Main(client <@TaxiPage.RenderPage()@>)
+            .Doc()
+    let Towing ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.NormalStyle)
+            .Navbar(Navbar.MakeNavbar ctx false)
+            .Main(client <@TowPage.RenderPage()@>)
+            .Doc()
+    let Delivery ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.NormalStyle)
+            .Navbar(Navbar.MakeNavbar ctx false)
+            .Main(client <@DeliveryPage.RenderPage()@>)
+            .Doc()
+    let AdminHome ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.AdminStyle)
+            .Navbar(Navbar.MakeNavbar ctx true)
+            .Main(Doc.Empty)
+            .Doc()
+    let CallsAdmin ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.AdminStyle)
+            .Navbar(Navbar.MakeNavbar ctx true)
+            .Main(client <@CallsAdmin.RenderPage()@>)
+            .Doc()
+    let ServiceAdmin ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.AdminStyle)
+            .Navbar(Navbar.MakeNavbar ctx true)
+            .Main(client <@ServiceFeeAdmin.RenderPage()@>)
+            .Doc()
+    let LoginPage ctx =
         SiteTemplates.LoginTemplate()
-                .Main(client <@renderer passtoRenderer@>)
-                .Doc()
+            .Main(client <@LoginPage.RenderPage()@>)
+            .Doc()
+    let RegisterPage ctx =
+        SiteTemplates.LoginTemplate()
+            .Main(client <@RegistrationPage.RenderPage()@>)
+            .Doc()
+    let InactivityPage ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.NormalStyle)
+            .Navbar(Navbar.MakeNavbar ctx false)
+            .Main(client <@InactivityPage.RenderPage()@>)
+            .Doc()
+    let NameChangeAdmin ctx =
+        SiteTemplates.MainTemplate()   
+            .Stylesheet(SiteTemplates.AdminStyle)
+            .Navbar(Navbar.MakeNavbar ctx true)
+            .Main(client <@NameChangeAdmin.RenderPage()@>)
+            .Doc()
+    let DocAdmin ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.AdminStyle)
+            .Navbar(Navbar.MakeNavbar ctx true)
+            .Main(client <@DocAdmin.RenderPage()@>)
+            .Doc()
+    let ImgAdmin ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.AdminStyle)
+            .Navbar(Navbar.MakeNavbar ctx true)
+            .Main(client <@ImageAdmin.RenderPage()@>)
+            .Doc()
+    let Logout (ctx:Context<EndPoint>) =
+        User.logoutUser ctx
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.NormalStyle)
+            .Navbar()
+            .Main(client <@LogoutClient.Logout()@>)
+            .Doc()
+    let NotFound ctx =
+        SiteTemplates.MainTemplate()
+            .Stylesheet(SiteTemplates.NormalStyle)
+            .Navbar(Navbar.MakeNavbar ctx false)
+            .Main(SiteParts.NotFoundTemplate().ErrorMessage("A keresett oldal nem található!").Doc())
+            .Doc()
+
--- src/server/Routing.fs
+++ src/server/Routing.fs
@@ -8,51 +8,43 @@ module Routing =
     let LoggedInRoute (user:Member) (ctx:Context<EndPoint>) endpoint =
         let sessionID = (ctx.Request.Cookies.Item "clms_sid").Value
         User.lengthenSession sessionID
-        let neededPermissions = Map.find endpoint Permission.RequiredPermissions
-        let needsAdmin = neededPermissions > ~~~Permissions.Admin && neededPermissions <> Permissions.Nothing
-        if not (Permission.checkPermission sessionID neededPermissions) then Content.RedirectTemporary(EndPoint.Home)
+        if not (Permission.checkPermission sessionID (Map.find endpoint Permission.RequiredPermissions)) then Content.RedirectTemporary(EndPoint.Home)
         else
         match endpoint with
-            |EndPoint.Home -> Content.Page(PageMakers.NormalPage ctx true false Information.RenderPage ctx)
-            |EndPoint.Logout -> 
-                    Content.Page(
-                        (fun () -> 
-                            User.logoutUser ctx
-                            PageMakers.NormalPage ctx false false LogoutClient.Logout ())()
-                    )
-            |EndPoint.PasswordChange -> Content.Page(PageMakers.NormalPage ctx false needsAdmin PasswordChangePage.RenderPage ())
-            |EndPoint.NameChange -> Content.Page(PageMakers.NormalPage ctx false needsAdmin NameChangePage.RenderPage ())
-            |EndPoint.Changelog -> Content.Page(PageMakers.NormalPage ctx true needsAdmin (fun () -> SiteParts.ChangelogTemplate().Doc()) ())
-            |EndPoint.CallsAdmin -> Content.Page(PageMakers.NormalPage ctx false needsAdmin CallsAdmin.RenderPage ())
-            |EndPoint.CarsAdmin -> Content.Page(PageMakers.NormalPage ctx false needsAdmin CarsAdmin.RenderPage ())
-            |EndPoint.Inactivity -> Content.Page(PageMakers.NormalPage ctx false needsAdmin InactivityPage.RenderPage ())
-            |EndPoint.Delivery -> Content.Page(PageMakers.NormalPage ctx false needsAdmin DeliveryPage.RenderPage ())
+            |EndPoint.Home -> Content.Page(PageMakers.Information ctx)
+            |EndPoint.Logout -> Content.Page(PageMakers.Logout ctx)
+            |EndPoint.PasswordChange -> Content.Page(PageMakers.PasswordChange ctx)
+            |EndPoint.NameChange -> Content.Page(PageMakers.NameChange ctx)
+            |EndPoint.Changelog -> Content.Page(PageMakers.Changelog ctx)
+            |EndPoint.AdminHome -> Content.Page(PageMakers.AdminHome ctx)
+            |EndPoint.CallsAdmin -> Content.Page(PageMakers.CallsAdmin ctx)
+            |EndPoint.CarsAdmin -> Content.Page(PageMakers.CarsAdmin ctx)
+            |EndPoint.Inactivity -> Content.Page(PageMakers.InactivityPage ctx)
+            |EndPoint.Delivery -> Content.Page(PageMakers.Delivery ctx)
             |EndPoint.Documents -> Content.Page(Documents.MakePage ctx)
             |EndPoint.DocumentSubmit -> ImageSubmitter.Documents ctx user
             |EndPoint.ImageSubmit -> ImageSubmitter.Images ctx user
             |EndPoint.ImageUpload -> Content.Page(ImageUpload.MakePage ctx)
-            |EndPoint.Information -> Content.Page(PageMakers.NormalPage ctx true needsAdmin Information.RenderPage ctx)
-            |EndPoint.MembersAdmin -> Content.Page(PageMakers.NormalPage ctx false needsAdmin MemberAdminPage.RenderPage user)
-            |EndPoint.InactivityAdmin -> Content.Page(PageMakers.NormalPage ctx false needsAdmin InactivityAdmin.RenderPage ())
-            |EndPoint.NameChangeAdmin -> Content.Page(PageMakers.NormalPage ctx false needsAdmin NameChangeAdmin.RenderPage ())
-            |EndPoint.RegistrationAdmin -> Content.Page(PageMakers.NormalPage ctx false needsAdmin RegistrationAdminPage.RenderPage ())
-            |EndPoint.ServiceAdmin -> Content.Page(PageMakers.NormalPage ctx false needsAdmin ServiceFeeAdmin.RenderPage ())
-            |EndPoint.DocAdmin -> Content.Page(PageMakers.NormalPage ctx false needsAdmin DocAdmin.RenderPage ())
-            |EndPoint.ImgAdmin -> Content.Page(PageMakers.NormalPage ctx false needsAdmin ImageAdmin.RenderPage ())
-            |EndPoint.Taxi -> Content.Page(PageMakers.NormalPage ctx false needsAdmin TaxiPage.RenderPage ())
-            |EndPoint.Towing -> Content.Page(PageMakers.NormalPage ctx false needsAdmin TowPage.RenderPage ())
+            |EndPoint.Information -> Content.Page(PageMakers.Information ctx)
+            |EndPoint.MembersAdmin -> Content.Page(PageMakers.MembersAdmin ctx user)
+            |EndPoint.NameChangeAdmin -> Content.Page(PageMakers.NameChangeAdmin ctx)
+            |EndPoint.RegistrationAdmin -> Content.Page(PageMakers.RegistrationAdmin ctx)
+            |EndPoint.ServiceAdmin -> Content.Page(PageMakers.ServiceAdmin ctx)
+            |EndPoint.DocAdmin -> Content.Page(PageMakers.DocAdmin ctx)
+            |EndPoint.ImgAdmin -> Content.Page(PageMakers.ImgAdmin ctx)
+            |EndPoint.Taxi -> Content.Page(PageMakers.Taxi ctx)
+            |EndPoint.Towing -> Content.Page(PageMakers.Towing ctx)
             |_ -> Content.RedirectTemporary(EndPoint.Information)
     let LoggedOutRoute (ctx:Context<EndPoint>) endpoint =
         match endpoint with
-            |EndPoint.Home -> Content.Page(PageMakers.LoginPage ctx LoginPage.RenderPage ())
-            |EndPoint.Login -> Content.Page(PageMakers.LoginPage ctx LoginPage.RenderPage ())
-            |EndPoint.Registration -> Content.Page(PageMakers.LoginPage ctx RegistrationPage.RenderPage ())
-            |EndPoint.ForgotPass -> Content.Page(PageMakers.LoginPage ctx ForgotPassPage.RenderPage ())
+            |EndPoint.Home -> Content.Page(PageMakers.LoginPage ctx)
+            |EndPoint.Login -> Content.Page(PageMakers.LoginPage ctx)
+            |EndPoint.Registration -> Content.Page(PageMakers.RegisterPage ctx)
             |_ -> Content.RedirectTemporary(EndPoint.Login)
 
     let MakeRoute (ctx:Context<EndPoint>) endpoint =
         match endpoint with
-            |NotFound _ -> Content.Page(PageMakers.NormalPage ctx true false (fun () -> SiteParts.NotFoundTemplate().ErrorMessage("A keresett oldal nem található!").Doc()) ())
+            |NotFound _ -> Content.Page(PageMakers.NotFound ctx)
             |DocServe fn -> ImageServe.Docs ctx fn
             |ImgServe fn -> ImageServe.Service ctx fn
             | _ ->
