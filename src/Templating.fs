7d3bcd183634da662c1f0099e00d8f754e8849d3
OrkenWhite <orkenwhite@disroot.org>
2022-08-10 11:19:46 +0200

    Moved templates out of wwwroot

--- src/Templating.fs
+++ src/Templating.fs
@@ -7,38 +7,38 @@ open WebSharper.UI.Templating
 //Full templates with html tags
 [<JavaScript>]
 module SiteTemplates =
-    let AdminStyle = "../adminstyle.css"
-    let NormalStyle = "normalstyle.css"
-    let ChangeStyle = "changestyles.css"
-    type MainTemplate = Templating.Template<"wwwroot/templates/main.html">
-    type LoginTemplate = Templating.Template<"wwwroot/templates/loginwrapper.html">
+    let AdminStyle = "/adminstyle.css"
+    let NormalStyle = "/normalstyle.css"
+    let ChangeStyle = "/changestyles.css"
+    type MainTemplate = Templating.Template<"templates/wrappers/main.html">
+    type LoginTemplate = Templating.Template<"templates/wrappers/loginwrapper.html">
 
 //Parts not serveable on their own
 [<JavaScript>]
 module SiteParts = 
-    type LoginPage = Templating.Template<"wwwroot/templates/login.html">
-    type RegistrationPage = Templating.Template<"wwwroot/templates/registration.html", serverLoad = ServerLoad.WhenChanged>
-    type AdminHomeTemplate = Templating.Template<"wwwroot/templates/admin.html">
-    type DeliveryTemplate = Templating.Template<"wwwroot/templates/delivery.html">
-    type CarTemplate = Templating.Template<"wwwroot/templates/cars.html">
-    type ChangelogTemplate = Templating.Template<"wwwroot/templates/changelog.html">
-    type ForgotPassTemplate = Templating.Template<"wwwroot/templates/forgotpass.html">
-    type ImageViewingTemplate = Templating.Template<"wwwroot/templates/imgview.html">
-    type DocViewingTemplate = Templating.Template<"wwwroot/templates/docview.html">
-    type CallsTemplate = Templating.Template<"wwwroot/templates/calls.html">
-    type InfoTemplate = Templating.Template<"wwwroot/templates/info.html">
-    type DocumentsTemplate = Templating.Template<"wwwroot/templates/docs.html">
-    type InactivityTemplate = Templating.Template<"wwwroot/templates/inactivity.html">
-    type InactivityAdminTemplate = Templating.Template<"wwwroot/templates/inactivity_admin.html">
-    type ImageUploadTemplate = Templating.Template<"wwwroot/templates/img.html">
-    type NavTemplate = Templating.Template<"wwwroot/templates/navbar.html">
-    type NameAdminTemplate = Templating.Template<"wwwroot/templates/name_admin.html">
-    type NameChangeTemplate = Templating.Template<"wwwroot/templates/namechange.html">
-    type PasswordChangeTemplate = Templating.Template<"wwwroot/templates/passchange.html"> 
-    type RegistrationAdminTemplate = Templating.Template<"wwwroot/templates/reg_admin.html">
-    type MemberListTemplate = Templating.Template<"wwwroot/templates/members.html">
-    type TaxiTemplate = Templating.Template<"wwwroot/templates/taxi.html">
-    type TowTemplate = Templating.Template<"wwwroot/templates/tow.html">
-    type ServiceTemplate = Templating.Template<"wwwroot/templates/service.html">
-    type NotFoundTemplate = Templating.Template<"wwwroot/templates/notfound.html">
+    type LoginPage = Templating.Template<"templates/parts/login.html">
+    type RegistrationPage = Templating.Template<"templates/parts/registration.html", serverLoad = ServerLoad.WhenChanged>
+    type AdminHomeTemplate = Templating.Template<"templates/parts/admin.html">
+    type DeliveryTemplate = Templating.Template<"templates/parts/delivery.html">
+    type CarTemplate = Templating.Template<"templates/parts/cars.html">
+    type ChangelogTemplate = Templating.Template<"templates/parts/changelog.html">
+    type ForgotPassTemplate = Templating.Template<"templates/parts/forgotpass.html">
+    type ImageViewingTemplate = Templating.Template<"templates/parts/imgview.html">
+    type DocViewingTemplate = Templating.Template<"templates/parts/docview.html">
+    type CallsTemplate = Templating.Template<"templates/parts/calls.html">
+    type InfoTemplate = Templating.Template<"templates/parts/info.html">
+    type DocumentsTemplate = Templating.Template<"templates/parts/docs.html">
+    type InactivityTemplate = Templating.Template<"templates/parts/inactivity.html">
+    type InactivityAdminTemplate = Templating.Template<"templates/parts/inactivity_admin.html">
+    type ImageUploadTemplate = Templating.Template<"templates/parts/img.html">
+    type NavTemplate = Templating.Template<"templates/parts/navbar.html">
+    type NameAdminTemplate = Templating.Template<"templates/parts/name_admin.html">
+    type NameChangeTemplate = Templating.Template<"templates/parts/namechange.html">
+    type PasswordChangeTemplate = Templating.Template<"templates/parts/passchange.html"> 
+    type RegistrationAdminTemplate = Templating.Template<"templates/parts/reg_admin.html">
+    type MemberListTemplate = Templating.Template<"templates/parts/members.html">
+    type TaxiTemplate = Templating.Template<"templates/parts/taxi.html">
+    type TowTemplate = Templating.Template<"templates/parts/tow.html">
+    type ServiceTemplate = Templating.Template<"templates/parts/service.html">
+    type NotFoundTemplate = Templating.Template<"templates/parts/notfound.html">
 
