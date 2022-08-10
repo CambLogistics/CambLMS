a8dc6bf5eed6cf4a7b0f6d6ec2d86616d241f9ad
OrkenWhite <orkenwhite@disroot.org>
2022-08-10 11:20:00 +0200

    Fixed connection string generation

--- src/server/Database.fs
+++ src/server/Database.fs
@@ -6,6 +6,7 @@ module Database =
     type SqlConnection = SqlDataProvider<Common.DatabaseProviderTypes.MYSQL,  "Server=localhost;Database=camblogistics;Uid=camblms;Pwd=V3l3tlen_J3lsz0;",UseOptionTypes = true,CaseSensitivityChange = Common.CaseSensitivityChange.TOLOWER>
     let getConnectionString() =
             let config = Config.readDatabase()
-            sprintf "Server=%s:%i;Database=%s;Uid=%s;Pwd=%s" config.Host config.Port config.DatabaseName config.Username config.Password
+            sprintf "Server=%s;Port=%i;Database=%s;Uid=%s;Pwd=%s" config.Host config.Port config.DatabaseName config.Username config.Password
     let getDataContext() =
+        let debugString = getConnectionString()
         SqlConnection.GetDataContext (getConnectionString())
