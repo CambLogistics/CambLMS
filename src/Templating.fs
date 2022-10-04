f6138c67e0ab0d5da70e898c9454ebdd20d2f4c7
OrkenWhite <orkenwhite@disroot.org>
2022-10-04 18:53:02 +0200

    Fixed template parsing errors

--- src/Templating.fs
+++ src/Templating.fs
@@ -17,20 +17,17 @@ module SiteTemplates =
 [<JavaScript>]
 module SiteParts = 
     type LoginPage = Templating.Template<"templates/parts/login.html">
-    type RegistrationPage = Templating.Template<"templates/parts/registration.html", serverLoad = ServerLoad.WhenChanged>
     type AdminHomeTemplate = Templating.Template<"templates/parts/admin.html">
     type CarTemplate = Templating.Template<"templates/parts/cars.html">
     type ChangelogTemplate = Templating.Template<"templates/parts/changelog.html">
     type ForgotPassTemplate = Templating.Template<"templates/parts/forgotpass.html">
-    type ImageViewingTemplate = Templating.Template<"templates/parts/imgview.html">
-    type DocViewingTemplate = Templating.Template<"templates/parts/docview.html">
-    type CallsTemplate = Templating.Template<"templates/parts/calls.html">
+    type ImageViewingTemplate = Templating.Template<"templates/parts/img.html">
+    type DocViewingTemplate = Templating.Template<"templates/parts/docsview.html">
     type InfoTemplate = Templating.Template<"templates/parts/info.html">
     type DocumentsTemplate = Templating.Template<"templates/parts/docs.html">
     type InactivityTemplate = Templating.Template<"templates/parts/inactivity.html">
     type InactivityAdminTemplate = Templating.Template<"templates/parts/inactivity_admin.html">
     type ImageUploadTemplate = Templating.Template<"templates/parts/img.html">
-    type NameAdminTemplate = Templating.Template<"templates/parts/name_admin.html">
     type NameChangeTemplate = Templating.Template<"templates/parts/namechange.html">
     type PasswordChangeTemplate = Templating.Template<"templates/parts/passchange.html"> 
     type RegistrationAdminTemplate = Templating.Template<"templates/parts/reg_admin.html">
