59b8d3791c713936339a465191de7d2764e0ea3f
OrkenWhite <orkenwhite@disroot.org>
2022-10-17 19:30:46 +0200

    Adapted PageMaker

--- src/server/PageMaker.fs
+++ src/server/PageMaker.fs
@@ -6,75 +6,75 @@ open WebSharper.UI.Html
 open WebSharper.Sitelets
 
 module PageMakers =
-    let RegistrationAdmin ctx =
+    let RegistrationAdmin ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.AdminStyle)
             .Navbar(Navbar.MakeNavbar ctx true)
+            .FirstName(name)
             .Main(client <@RegistrationAdminPage.RenderPage()@>)
             .Doc()
-    let PasswordChange ctx =
+    let PasswordChange ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.ChangeStyle)
             .Navbar(Navbar.MakeNavbar ctx false)
+            .FirstName(name)
             .Main(client <@PasswordChangePage.RenderPage()@>)
             .Doc()
-    let NameChange ctx =
+    let NameChange ctx (name:string) =
          SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.ChangeStyle)
             .Navbar(Navbar.MakeNavbar ctx false)
+            .FirstName(name)
             .Main(client <@NameChangePage.RenderPage()@>)
             .Doc()
-    let MembersAdmin ctx user =
+    let MembersAdmin ctx user (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.AdminStyle)
             .Navbar(Navbar.MakeNavbar ctx true)
+            .FirstName(name)
             .Main(client <@MemberAdminPage.RenderPage user@>)
             .Doc()
-    let Changelog ctx =
+    let Changelog ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.NormalStyle)
             .Navbar(Navbar.MakeNavbar ctx false)
+            .FirstName(name)
             .Main(SiteParts.ChangelogTemplate().Doc())
             .Doc()
-    let CarsAdmin ctx =
+    let CarsAdmin ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.AdminStyle)
             .Navbar(Navbar.MakeNavbar ctx true)
+            .FirstName(name)
             .Main(client <@CarsAdmin.RenderPage()@>)
             .Doc()
-    let Information ctx =
+    let Information ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.NormalStyle)
             .Navbar(Navbar.MakeNavbar ctx false)
+            .FirstName(name)
             .Main(Information.RenderPage ctx)
             .Doc()
-    let Taxi ctx =
+    let Taxi ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.NormalStyle)
             .Navbar(Navbar.MakeNavbar ctx false)
+            .FirstName(name)
             .Main(client <@TaxiPage.RenderPage()@>)
             .Doc()
-    let Towing ctx =
+    let Towing ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.NormalStyle)
             .Navbar(Navbar.MakeNavbar ctx false)
+            .FirstName(name)
             .Main(client <@TowPage.RenderPage()@>)
             .Doc()
-    let AdminHome ctx =
+    let AdminHome ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.AdminStyle)
             .Navbar(Navbar.MakeNavbar ctx true)
+            .FirstName(name)
             .Main(Doc.Empty)
             .Doc()
-    let CallsAdmin ctx =
+    let CallsAdmin ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.AdminStyle)
+            .FirstName(name)
             .Navbar(Navbar.MakeNavbar ctx true)
             .Main(client <@CallsAdmin.RenderPage()@>)
             .Doc()
-    let ServiceAdmin ctx =
+    let ServiceAdmin ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.AdminStyle)
+            .FirstName(name)
             .Navbar(Navbar.MakeNavbar ctx true)
             .Main(client <@ServiceFeeAdmin.RenderPage()@>)
             .Doc()
@@ -86,34 +86,34 @@ module PageMakers =
         SiteTemplates.LoginTemplate()
             .Main(client <@RegistrationPage.RenderPage()@>)
             .Doc()
-    let InactivityPage ctx =
+    let InactivityPage ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.ChangeStyle)
             .Navbar(Navbar.MakeNavbar ctx false)
+            .FirstName(name)
             .Main(client <@InactivityPage.RenderPage()@>)
             .Doc()
-    let InactivityAdmin ctx =
+    let InactivityAdmin ctx (name:string) =
         SiteTemplates.MainTemplate()   
-            .Stylesheet(SiteTemplates.AdminStyle)
             .Navbar(Navbar.MakeNavbar ctx true)
+            .FirstName(name)
             .Main(client <@InactivityAdmin.RenderPage()@>)
             .Doc()
-    let NameChangeAdmin ctx =
-        SiteTemplates.MainTemplate()   
-            .Stylesheet(SiteTemplates.AdminStyle)
+    let NameChangeAdmin ctx (name:string) =
+        SiteTemplates.MainTemplate()
+            .FirstName(name) 
             .Navbar(Navbar.MakeNavbar ctx true)
             .Main(client <@NameChangeAdmin.RenderPage()@>)
             .Doc()
-    let DocAdmin ctx =
+    let DocAdmin ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.AdminStyle)
             .Navbar(Navbar.MakeNavbar ctx true)
+            .FirstName(name)
             .Main(client <@DocAdmin.RenderPage()@>)
             .Doc()
-    let ImgAdmin ctx =
+    let ImgAdmin ctx (name:string) =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.AdminStyle)
             .Navbar(Navbar.MakeNavbar ctx true)
+            .FirstName(name)
             .Main(client <@ImageAdmin.RenderPage()@>)
             .Doc()
     let ForgotPass ctx =
@@ -123,14 +123,13 @@ module PageMakers =
     let Logout (ctx:Context<EndPoint>) =
         User.logoutUser ctx
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.NormalStyle)
             .Navbar()
             .Main(client <@LogoutClient.Logout()@>)
             .Doc()
     let NotFound ctx =
         SiteTemplates.MainTemplate()
-            .Stylesheet(SiteTemplates.NormalStyle)
-            .Navbar(Navbar.MakeNavbar ctx false)
+            .FirstName("látogató")
+            .Navbar()
             .Main(SiteParts.NotFoundTemplate().ErrorMessage("A keresett oldal nem található!").Doc())
             .Doc()
 
