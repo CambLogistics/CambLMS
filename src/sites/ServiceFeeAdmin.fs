94843b35ec0600eb3a5f32007bba3bac32657d4e
OrkenWhite <orkenwhite@disroot.org>
2022-10-18 18:42:31 +0200

    Moved service image administration to service admin

--- src/sites/ServiceFeeAdmin.fs
+++ src/sites/ServiceFeeAdmin.fs
@@ -6,53 +6,75 @@ open WebSharper.UI.Client
 
 [<JavaScript>]
 module ServiceFeeAdmin =
-    let UserList = ListModel.Create (fun (u:Member) -> u) []
-    let PendingList = ListModel.Create (fun (f:PendingFee) -> f) []
-    let SelectedUserID = Var.Create -1
+    let OwedUserList = ListModel.Create (fun (u:Member) -> u) []
+    let PendingFeeList = ListModel.Create (fun (f:PendingFee) -> f) []
+    let SelectedOwedUserID = Var.Create -1
+    let ImageUserList = ListModel.Create (fun (u:Member) -> u) []
+    let ImageList = ListModel.Create (fun (i:string*int*System.DateTime) -> i) []
+    let SelectedImageUploader = Var.Create -1
+    let currentImages = View.Map (fun il -> il |> Seq.filter (fun (_,uid,_) -> uid = SelectedImageUploader.Value)) ImageList.View
     let sessionID = JavaScript.Cookies.Get("clms_sid").Value
-    let updateUserList() =
+    let updateImageLists() =
+        async{
+            let! userlist = UserOperations.getUserList sessionID false true
+            let! imagelist = ImageUpload.getImageList sessionID
+            match imagelist with
+                |Ok l ->
+                    ImageList.Set l
+                    match userlist with
+                        |Ok ul ->
+                            query{
+                                for (fn,uploader,_) in l do
+                                join u in ul on (uploader = u.Id)
+                                select u
+                            } |> ImageUserList.Set
+                            if SelectedImageUploader.Value = -1 && not <| Seq.isEmpty ImageUserList.Value then SelectedImageUploader.Set <| (Seq.head ImageUserList.Value).Id
+                        |Error e ->  Feedback.giveFeedback true <| "Hiba a felhasználó lista lekérésekor: " + e
+                |Error e -> Feedback.giveFeedback true <| "Hiba a képlista lekérésekor: " + e
+        } |> Async.Start
+    let updateOwedUserList() =
         async{
             let! list = UserOperations.getUserList sessionID false false
             match list with
                 |Ok l ->
-                    UserList.Set l
-                    SelectedUserID.Set (Seq.item 0 UserList.Value).Id
+                    OwedUserList.Set l
+                    SelectedOwedUserID.Set (Seq.item 0 OwedUserList.Value).Id
                 |Error e ->
                     Feedback.giveFeedback true <| "Hiba a felhasználók lekérdezésekor: " + e
         } |> Async.Start
-    let updatePendingList() =
+    let updatePendingFeeList() =
         async{
             let! list = ServiceFee.getPendingFees sessionID 
             match list with
                 |Ok l ->
-                    PendingList.Set l
-                    SelectedUserID.Set (Seq.item 0 UserList.Value).Id
+                    PendingFeeList.Set l
+                    SelectedOwedUserID.Set (Seq.item 0 OwedUserList.Value).Id
                 |Error e ->
                     Feedback.giveFeedback true <| "Hiba a szervizdíjak lekérdezésekor: " + e
         } |> Async.Start
     let RenderPage() =
-        updateUserList()
-        updatePendingList()
+        updateOwedUserList()
+        updatePendingFeeList()
         SiteParts.ServiceTemplate()
-            .MemberList(
-                UserList.View |> Doc.BindSeqCached(
+            .OwedMemberList(
+                OwedUserList.View |> Doc.BindSeqCached(
                     fun u ->
-                        SiteParts.ServiceTemplate.MemberItem()
+                        SiteParts.ServiceTemplate.OwedMemberItem()
                             .UserID(string u.Id)
                             .Member(u.Name)
                             .Doc()
                 )
             )
-            .MemberSelectID(SelectedUserID.Lens (fun id -> string id) (fun id s -> int s))
+            .MemberSelectID(SelectedOwedUserID.Lens (fun id -> string id) (fun id s -> int s))
             .ServiceList(
-                PendingList.View |> Doc.BindSeqCached(
+                PendingFeeList.View |> Doc.BindSeqCached(
                     fun p ->
                         SiteParts.ServiceTemplate.ServiceListItem()
                             .ID(string p.ID)
                             .Name(p.Username)
                             .Paid(
                                 fun e ->
-                                    ActionDispatcher.RunAction ServiceFee.payFee (sessionID, p.ID) (Some updatePendingList)
+                                    ActionDispatcher.RunAction ServiceFee.payFee (sessionID, p.ID) (Some updatePendingFeeList)
                             )
                             .ServiceFee((string p.Amount) + "$")
                             .Doc()
@@ -62,6 +84,32 @@ module ServiceFeeAdmin =
                 fun e ->
                         if JavaScript.JS.IsNaN e.Vars.Fee.Value then Feedback.giveFeedback true "Kérlek számot adj meg árnak!"
                         else
-                            ActionDispatcher.RunAction ServiceFee.submitPendingFee (sessionID,(int e.Vars.MemberSelectID.Value), (int e.Vars.Fee.Value)) (Some updatePendingList)
+                            ActionDispatcher.RunAction ServiceFee.submitPendingFee (sessionID,(int e.Vars.MemberSelectID.Value), (int e.Vars.Fee.Value)) (Some updatePendingFeeList)
+            )
+             .OnSelection(
+                fun _ -> updateImageLists()
+            )
+            .UploaderList(
+                Doc.BindSeqCached(
+                    fun (u:Member) ->
+                        SiteParts.ServiceTemplate.UploaderItem()
+                            .MemberID(string u.Id)
+                            .MemberName(u.Name)
+                            .Doc()
+                ) ImageUserList.View
+            )
+            .SelectedUploader(SelectedImageUploader.Lens (fun m -> string m) (fun m mi -> int mi))
+            .ImageList(
+                Doc.BindSeqCached (
+                    fun (fn,uploader,(date:System.DateTime)) ->
+                        SiteParts.ServiceTemplate.ImageItem()
+                            .ImageLink("/img/" + fn)
+                            .UploadDate(string date.Year + "-" + sprintf "%02d" date.Month + "-" + sprintf "%02d" date.Day + " " + sprintf "%02d" date.Hour + ":" + sprintf "%02d" date.Minute)
+                            .Delete(
+                                fun e ->
+                                    ActionDispatcher.RunAction ImageUpload.DeleteImage (sessionID,fn) (Some updateImageLists)
+                            )
+                            .Doc()
+                ) currentImages
             )
             .Doc()
