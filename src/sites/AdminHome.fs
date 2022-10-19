cb2e63f692a6776ac8e3e370445ea9811e63b425
OrkenWhite <orkenwhite@disroot.org>
2022-10-19 22:13:23 +0200

    Added admin home functionalities

--- src/sites/AdminHome.fs
+++ src/sites/AdminHome.fs
@@ -6,18 +6,48 @@ open WebSharper.UI.Client
 
 [<JavaScript>]
 module AdminHome =
-    let userList = ListModel.Create (fun (u:UserWithCalls) -> u) []
+    let weeklyUserList = ListModel.Create (fun (u:UserWithCalls) -> u) []
+    let topList = ListModel.Create (fun (u:UserWithCalls*int) -> u) []
+    let allTaxis = Var.Create 0
+    let occupiedTaxis = Var.Create 0
+    let allTowTrucks = Var.Create 0
+    let occupiedTowTrucks = Var.Create 0
     let sessionID = JavaScript.Cookies.Get("clms_sid").Value
     let rankList = Var.Create [{Level = 0;Name="Beszállító"}]
     let mutable canClose = false
-    let updateUserList() =
+    let updateWeeklyUserList() =
         async{
             let! list = Calls.getUserListWithCalls sessionID CallDuration.Weekly
             let! cc = Permission.doCheckPermission sessionID Permissions.CloseWeek
             canClose <- cc
-            if cc then JavaScript.JS.Document.GetElementById("closeweek").RemoveAttribute("style")
+            if not cc then JavaScript.JS.Document.GetElementById("closeweek").SetAttribute("style","display: none;")
             match list with
-                |Ok l -> l |> userList.Set
+                |Ok l -> l |> weeklyUserList.Set
+                |Error e -> Feedback.giveFeedback true e
+        } |> Async.Start
+    let updateCarData() =
+        async{
+            let! taxiStat = Cars.getCarCountByWorkType sessionID CarWorkType.Taxi
+            let! towingStat = Cars.getCarCountByWorkType sessionID CarWorkType.Tow
+            match taxiStat with
+                |Ok (all,occupied) ->
+                    allTaxis.Set all
+                    occupiedTaxis.Set occupied
+                    match towingStat with
+                        |Ok(tAll,tOcc) ->
+                            allTowTrucks.Set tAll
+                            occupiedTowTrucks.Set tOcc
+                        |Error e -> Feedback.giveFeedback true e
+                |Error e -> Feedback.giveFeedback true e
+        } |> Async.Start
+    let updateTopList() =
+        async{
+            let! list = Calls.getUserListWithCalls sessionID CallDuration.TopList
+            let! cc = Permission.doCheckPermission sessionID Permissions.CloseWeek
+            canClose <- cc
+            if not cc then JavaScript.JS.Document.GetElementById("closemonth").SetAttribute("style","display: none;")
+            match list with
+                |Ok l -> l |> List.map (fun u -> (u,u.Calls |> List.map (fun c -> c.Price) |> List.sum)) |> List.sortByDescending (fun (_,money) -> money) |> topList.Set
                 |Error e -> Feedback.giveFeedback true e
         } |> Async.Start
     let updateRankList() =
@@ -29,10 +59,12 @@ module AdminHome =
         } |> Async.Start
     let RenderPage() =
         updateRankList()
-        updateUserList()
+        updateWeeklyUserList()
+        updateTopList()
+        updateCarData()
         SiteParts.AdminHomeTemplate()
             .CallMemberList(
-                userList.View |> Doc.BindSeqCached (
+                weeklyUserList.View |> Doc.BindSeqCached (
                     fun u ->
                         SiteParts.AdminHomeTemplate.CallMember()
                             .Name(u.User.Name)
@@ -47,10 +79,31 @@ module AdminHome =
                             .Doc()
                 )
             )
+            .TopMemberList(
+                topList.View |> Doc.BindSeqCached (
+                    fun (u,m) ->
+                        SiteParts.AdminHomeTemplate.TopMember()
+                            .Name(u.User.Name)
+                            .CallMonth(string <| List.length u.Calls)
+                            .MonthMoney(string m)
+                            .Doc()
+                )
+            )
+            .AllTaxis(string allTaxis.Value)
+            .AllTowingTrucks(string allTowTrucks.Value)
+            .TaxiCar(string occupiedTaxis.Value)
+            .TowTruck(string occupiedTowTrucks.Value)
+            .CashSum(weeklyUserList.Value |> Seq.map (fun uc -> uc.Calls) |> Seq.map (fun cl -> cl |> List.map (fun c -> c.Price)) |> Seq.map (fun p -> List.sum p) |> Seq.sum |> string)
             .CloseWeek(
                 fun _ ->
                     if not canClose then ()
                     else
-                        ActionDispatcher.RunAction Calls.rotateWeek sessionID (Some updateUserList)
+                        ActionDispatcher.RunAction Calls.rotateWeek sessionID (Some updateWeeklyUserList)
+            )
+            .CloseMonth(
+                 fun _ ->
+                    if not canClose then ()
+                    else
+                        ActionDispatcher.RunAction Calls.rotateTopList sessionID (Some updateWeeklyUserList)
             )
             .Doc()
