6ad7db0890060136c636bb873aaf573c66e85a7a
OrkenWhite <orkenwhite@disroot.org>
2022-10-19 23:18:46 +0200

    Completed blacklist functionality

--- src/server/Blacklist.fs
+++ src/server/Blacklist.fs
@@ -5,7 +5,7 @@ open WebSharper
 [<JavaScript>]
 type BlacklistItem =
     {
-        Name: string
+        UserName: string
         Role: int
         AccountID: int
         Reason: string
@@ -23,7 +23,7 @@ module Blacklist =
             return Ok (query {
                 for i in db.Camblogistics.blacklist do
                     select{
-                        Name = i.Name
+                        UserName = i.Name
                         Role = i.RoleId
                         AccountID = i.AccountId
                         Reason = i.Reason
@@ -33,7 +33,7 @@ module Blacklist =
         with e -> return Error e.Message
         }
     [<Rpc>]
-    let setBlackListItem sid bli =
+    let setBlackListItem (sid,bli) =
         async{
         try
             if not (Permission.checkPermission sid Permissions.MemberAdmin) then return ActionResult.InsufficientPermissions
@@ -48,10 +48,29 @@ module Blacklist =
                 newItem.AccountId <- bli.AccountID
                 newItem.CanReturn <- if bli.Comeback then sbyte 1 else sbyte 0
                 newItem.Reason <- bli.Reason
-                newItem.Name <- bli.Name
+                newItem.Name <- bli.UserName
                 newItem.RoleId <- bli.Role
                 db.SubmitUpdates()
                 return ActionResult.Success
 
         with e -> return ActionResult.DatabaseError
+        }
+    [<Rpc>]
+    let deleteItem (sid,bli) =
+        async{
+            try
+                if not (Permission.checkPermission sid Permissions.MemberAdmin) then return ActionResult.InsufficientPermissions
+                else
+                    let db = Database.getDataContext()
+                    let existing = query {
+                        for i in db.Camblogistics.blacklist do
+                            where(i.AccountId = bli.AccountID)
+                            select i
+                    }
+                    if Seq.length existing = 0 then return ActionResult.OtherError "Nem lehet nem létező feketelista elemet törölni!"
+                    else
+                        (Seq.item 0 existing).Delete()
+                        db.SubmitUpdates()
+                        return ActionResult.Success
+            with e -> return ActionResult.DatabaseError
         }
--- src/sites/MemberAdminPage.fs
+++ src/sites/MemberAdminPage.fs
@@ -8,6 +8,8 @@ open WebSharper.UI.Client
 module MemberAdminPage =
     let UserList = ListModel.Create (fun (u:Member) -> u) []
     let RankList = ListModel.FromSeq [ { Level = -1; Name = "???" } ]
+    let BlackList = ListModel.Create (fun (bli:BlacklistItem) -> bli) []
+    let selectedBlackListItem = Var.Create {AccountID = 0;UserName = ""; Role = 0;Reason = ""; Comeback = false}
     let sessionID = JavaScript.Cookies.Get("clms_sid").Value
     let updateRankList() =
         async {
@@ -28,10 +30,20 @@ module MemberAdminPage =
                     Feedback.giveFeedback true <| "Hiba a felhasználók lekérésekor: " + e
         }
         |> Async.Start
+    let updateBlacklist() =
+        async{
+            let! list = Blacklist.getBlackListItems sessionID
+            match list with
+                |Ok l ->
+                    BlackList.Set l
+                |Error e ->
+                    Feedback.giveFeedback true <| "Hiba a feketelista lekérdezésekor: " + e 
+        } |> Async.Start
     
-    let RenderPage currentUser =
+    let RenderPage (currentUser:Member) =
         updateRankList()
         updateUserList()
+        updateBlacklist()
         SiteParts
             .MemberListTemplate()
             .ActiveUserList(
@@ -93,10 +105,46 @@ module MemberAdminPage =
                                     .MemberListTemplate
                                     .DeleteButton()
                                     .DeleteUser(fun e ->
+                                            selectedBlackListItem.Set {AccountID = u.AccountID;Role = u.Role;UserName = u.Name;Reason="";Comeback=false}
                                             ActionDispatcher.RunAction UserOperations.deleteUser (sessionID,u.Id) (Some updateUserList)
                                     )
                                     .Doc()
                         )
                         .Doc())
+            )
+            .BLAccID(selectedBlackListItem.Lens (fun i -> string i.AccountID) (fun i s -> {i with AccountID = int s}))
+            .BLRank(selectedBlackListItem.Lens (fun i -> string i.Role) (fun i s -> {i with Role = int s}))
+            .BLName(selectedBlackListItem.LensAuto (fun i -> i.UserName))
+            .BLComeBack(selectedBlackListItem.LensAuto (fun i -> i.Comeback))
+            .BLReason(selectedBlackListItem.LensAuto (fun i -> i.Reason))
+            .RemovedUserList(
+                BlackList.View |> Doc.BindSeqCached (
+                    fun bli ->
+                        SiteParts.MemberListTemplate.RemovedUserListItem()
+                            .AccID(string bli.AccountID)
+                            .ComeBack(if bli.Comeback then "igen" else "nem")
+                            .Name(bli.UserName)
+                            .Reason(bli.Reason)
+                            .Rank(
+                                (query {
+                                    for r in RankList do
+                                        where(r.Level = bli.Role)
+                                        exactlyOne
+                                }).Name
+                            )
+                            .Edit(
+                                fun _ ->
+                                    selectedBlackListItem.Set bli
+                            )
+                            .Remove(
+                                fun _ ->
+                                    ActionDispatcher.RunAction Blacklist.deleteItem (sessionID,bli) (Some updateBlacklist)
+                            )
+                            .Doc()
+                )
+            )
+            .BLConfirm(
+                fun _ ->
+                    ActionDispatcher.RunAction Blacklist.setBlackListItem (sessionID,selectedBlackListItem.Value) (Some updateBlacklist)
             )
             .Doc()
