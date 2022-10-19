a5de6f2a6a9882593c896a03e499c4c8f05d1679
OrkenWhite <orkenwhite@disroot.org>
2022-10-19 19:02:39 +0200

    Merged registration with login

--- src/sites/LoginPage.fs
+++ src/sites/LoginPage.fs
@@ -17,6 +17,21 @@ module LoginPage =
             | LoginResult.NotApproved ->
                 Feedback.giveFeedback true "Regisztrációd még nem lett jóváhagyva. Kérjük légy türelemmel!"
         }
+    let register name password accid email =
+        async {
+            if not (JS.IsNaN accid) then
+                let! result = User.registerUser(name,password,(int accid),email)
+                match result with
+                | RegisterResult.Success -> JS.Window.Location.Replace "/login?registered=true"
+                | Exists -> Feedback.giveFeedback true "Ilyen felhasználó már létezik!"
+                | RegisterResult.DatabaseError ->
+                    Feedback.giveFeedback true "Adatbázishiba. Kérjük értesítsd a (műszaki) igazgatót!"
+                | MissingData -> Feedback.giveFeedback true "Ellenőrizd a bevitt adatokat!"
+                | BadName -> Feedback.giveFeedback true "Nem megfelelő név!"
+                | BadPassword -> Feedback.giveFeedback true "Nem megfelelő jelszó!"
+            else
+                Feedback.giveFeedback true "Ellenőrizd a bevitt adatokat!"
+        }
     let RenderPage () =
         if JS.Window.Location.Href.Contains "registered" then
             Feedback.giveFeedback false "Regisztrációd sikeres és hamarosan jóváhagyásra kerül!"
@@ -29,4 +44,19 @@ module LoginPage =
                 fun e ->
                     if e.Event.KeyCode = 13 then callLogin e.Vars.Name.Value e.Vars.Password.Value |> Async.Start
             )
+            .Kbregister(fun e ->
+                if e.Event.KeyCode = 13 then
+                    register
+                        e.Vars.Name.Value
+                        e.Vars.Password.Value
+                        e.Vars.AccID.Value
+                        e.Vars.Email.Value
+                    |> Async.Start)
+            .Register(fun e ->
+                register
+                    e.Vars.Name.Value
+                    e.Vars.Password.Value
+                    e.Vars.AccID.Value
+                    e.Vars.Email.Value
+                |> Async.Start)
             .Doc()
